fc7300eb92cf9b2ad4eb6575e4cbfd2c86ade2d2
AV3S <av3stube@gmail.com>
2016-12-07 22:29:13 +0100

    Initialization
    Class Game, game loop, class Snake.

--- Program.cs
+++ Program.cs
@@ -4,12 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleSnakeGH
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace ConsoleSnakeGH {
+    class Snake {
+        enum direction { UP, RIGHT, DOWN, LEFT };
+
+        struct point {
+            public int x;
+            public int y;
+            public bool visible;
+        }
+    }
+
+    class Game {
+
+        enum direction { UP, RIGHT, DOWN, LEFT };
+
+        static void Main( string[] args ) {
+            bool done = false;
+            int test = 0;
+            float FPS = 2.0f;
+
+            while ( !done )
+            {
+                System.Threading.Thread.Sleep( (int)( 1000.0f / FPS ) );
+                Console.WriteLine(test);
+                test++;
+            }
         }
     }
 }
